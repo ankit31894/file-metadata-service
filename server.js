9a4e995300d09d9092782cf89fb9425746afd92b
Ankit <ankit31894@gmail.com>
2016-06-26 21:56:37 +0530

    Fixed routing

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ app.post('/api/upload',function(req,res){
     });
 });
 
-app.listen(process.env.port||3000,function(){
+app.listen(process.env.PORT||3000,function(){
     console.log("Working on port 3000");
 });
